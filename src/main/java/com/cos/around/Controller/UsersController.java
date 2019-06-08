c9e69b3ae56570a70dba1878b45fc59a6d268e5f
noinel <rainbow0043@naver.com>
2019-06-09 02:14:33 +0900

    update file

--- src/main/java/com/cos/around/Controller/UsersController.java
+++ src/main/java/com/cos/around/Controller/UsersController.java
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.around.Model.Users;
-
 import com.cos.around.Repository.UsersRepository;
-import com.cos.around.Utils.MyUtils;
 
 @RestController
 @RequestMapping("/users")
@@ -25,7 +23,7 @@ public class UsersController {
 
 	@PostMapping("/save")
 	public Users save(@RequestBody Users user) {
-
+		System.out.println(user);
 		user.setUserActivate(1);
 	
 		return userRepository.save(user);
